126c0949ef3f05fb71b686425576c059f326ca51
Ayush Soni <ayushkumar542004@gmail.com>
2026-03-03 21:26:29 +0530

    Updated
    Removed functions for calculating hash and simhash, along with related frequency analysis. Updated fetchdetails function to only handle a single URL.

--- main.py
+++ main.py
@@ -2,38 +2,10 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-
-# i am writing a 64 bit hash function for a word using polynomial rolling hash function
-def bithashf(word):
-    p=53
-    m=2**64
-    hashvalue=0
-    for i in range(len(word)):
-        asciivalue=ord(word[i])
-        hashvalue+=asciivalue*(p**i)
-    return hashvalue%m
-
-
-# Now writing function that will calculate simhash by taking wordfreq dicitionary as input
-def calculatesimhash(wordfreq):
-    array = [0]*64
-    for word in wordfreq:
-        hashofword=bithashf(word)
-        frequency=wordfreq[word]
-        for i in range(64):
-            if (hashofword>>i) & 1:
-                array[i]+=frequency
-            else:
-                array[i]-=frequency
-
-    result=0
-    for i in range(64):
-        if array[i]>0:
-            result = result | (1<<i)
-    return result
-
 # writting a fucntion which will calculate evrything requred and will print only if we wanted it to , i will be using this function for handling only one link case and two link case.
 def fetchdetails(url, printdetails=True):
+    if not url.startswith("https://"):
+        url = "https://" + url
 
     link = url
     fetchresponse=requests.get(link)
@@ -77,49 +49,7 @@ def fetchdetails(url, printdetails=True):
 
     if printdetails:
         print("\n")
-
-    # Now i am going to work on words and its frequencies
-    htmlbody=htmlbody.lower()
-    wordlist=[]
-    currword=""
-    for ch in htmlbody:
-        if ch.isalnum():
-            currword+=ch
-        else:
-            if currword!="":
-                wordlist.append(currword)
-                currword=""
-    if currword!="":
-        wordlist.append(currword)
-
-    # creating a dictionary that will store the word as key and its frequency as value
-    wordfreq={}
-    for word in wordlist:
-        if word in wordfreq.keys():
-            wordfreq[word]+=1
-        else:
-            wordfreq[word]=1
-
-    if printdetails:
-        print("Printing some words and their frequencies for testing example")
-        print("Word :   Frequency\n")
-        for i in range(min(4,len(wordlist))):
-            print(f"{wordlist[i]}    {wordfreq[wordlist[i]]}")
-        print("\n")
-
-        print("Printing some hashvalues of some words as example")
-        print("Word :     Hash Value")
-        for i in range(min(4,len(wordlist))):
-            print(f"{wordlist[i]}    {bithashf(wordlist[i])}")
-
-    simhashvalue = calculatesimhash(wordfreq)
-
-    if printdetails:
-        print("Simhash Value :")
-        print(simhashvalue)
-
-    return simhashvalue
-
+        
 
 if __name__=="__main__":
 
@@ -131,20 +61,5 @@ if __name__=="__main__":
     elif len(sys.argv)==2:
         fetchdetails(sys.argv[1], True)
 
-    # if two links are entered then i will print only the difference between the simhash values 
-    elif len(sys.argv)==3:
-        simhash1 = fetchdetails(sys.argv[1], False)
-        simhash2 = fetchdetails(sys.argv[2], False)
-
-        xorvalue = simhash1 ^ simhash2
-        differentbits =0
-        while xorvalue:
-            if xorvalue & 1==1:
-                differentbits+=1
-            xorvalue=xorvalue>>1    
-        commonbits = 64 - differentbits
-
-        print("Common Bits in both the simhash values is:", commonbits)
-
     else:
-        print("Too many links are enterd, please enter either one or two links separated by tab")
+        print("Too many links are enterd, please enter one")
